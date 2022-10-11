aa64f3c29945838576f36804abfa55e8df8630b8
Chris Liu <38637433+MenghsuanLiu@users.noreply.github.com>
2022-10-11 15:25:55 +0800

    Create ExtendedPageClasses_BaseForm.cs

---
+++ ExtendedPageClasses_BaseForm.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace ChrisWeb.Web.ExtendedPageClasses
+{
+     public abstract class BaseForm : Page
+    {        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            #region Set No Cache...
+            Response.Cache.SetCacheability(HttpCacheability.Private);
+            Response.Cache.SetExpires(DateTime.Parse("1970/1/1"));
+            Response.Cache.SetNoStore();
+            #endregion
+
+            //if (!IsPostBack)
+            //{
+            //    LoadFormData();
+            //}
+        }
+
+        #region - 繼承後必須實作的抽象method
+        /// <summary>
+        /// 自行定義 載入表單資料該做的事
+        /// </summary>
+        public abstract void LoadFormData();
+
+        public abstract void BindBasicInfo();
+        #endregion
+
+
+    }
+}
